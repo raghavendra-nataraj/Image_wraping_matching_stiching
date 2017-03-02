c85564cbdc056238eaa4e43ee3ed5c89362aa63c
ragha <ragha@ragha.com>
2017-03-02 15:43:19 -0500

    Part 1 2

--- a2.cpp
+++ a2.cpp
@@ -32,11 +32,21 @@ struct lines {
   double y2;
 };
 
+struct matStr {
+  string fileName;
+  int matches;
+};
+
 bool sortPoint(point p1,point p2){
   return p1.score<p2.score;
 }
 
-void match2Images(CImg<double> &input_image,CImg<double> &input_image2){
+
+bool sortImages(matStr i1,matStr i2){
+  return i1.matches>i2.matches;
+}
+
+int match2Images(CImg<double> &input_image,CImg<double> &input_image2){
   CImg<double> final_image;
   CImg<double> gImg1 = input_image.get_RGBtoHSI().get_channel(2);
   CImg<double> gImg2 = input_image2.get_RGBtoHSI().get_channel(2);
@@ -44,6 +54,7 @@ void match2Images(CImg<double> &input_image,CImg<double> &input_image2){
   final_image.append(input_image2);
   vector<SiftDescriptor> img1 = Sift::compute_sift(gImg1);
   vector<SiftDescriptor> img2 = Sift::compute_sift(gImg2);
+  int matches=0;
   for(int i=0; i<img1.size(); i++)
     {
       vector<point> scores;
@@ -59,12 +70,14 @@ void match2Images(CImg<double> &input_image,CImg<double> &input_image2){
       sort(scores.begin(),scores.end(),sortPoint);
       point p1 = scores[0];
       point p2 = scores[1];
-      if(p1.score/p2.score<0.5){
+      if(p1.score/p2.score<0.7){
 	const unsigned char color[] = { 255,128,64 };
 	final_image.draw_line(img1[i].col,img1[i].row,input_image.width()+p1.x,p1.y,color);
+	matches+=1;
       }
     }
   final_image.save("result.png");
+  return matches;
 }
 
 int main(int argc, char **argv)
@@ -80,34 +93,26 @@ int main(int argc, char **argv)
 
     string part = argv[1];
     string inputFile = argv[2];
-    string inputFile2 = argv[3];
     if(part == "part1")
       {
 	// This is just a bit of sample code to get you started, to
 	// show how to use the SIFT library.
-
+	vector<matStr> matC;
 	CImg<double> input_image(inputFile.c_str());
-	CImg<double> input_image2(inputFile2.c_str());
-	// convert image to grayscale
-	match2Images(input_image,input_image2);
-	/*vector<SiftDescriptor> descriptors = Sift::compute_sift(gray);
-		cout<<"Size="<<descriptors.size()<<endl;
-		for(int i=0; i<descriptors.size(); i++)
-	  {
-	    cout << "Descriptor #" << i << ": x=" << descriptors[i].col << " y=" << descriptors[i].row << " descriptor=(";
-	    for(int l=0; l<128; l++)
-	      cout << descriptors[i].descriptor[l] << "," ;
-	    cout << ")" << endl;
-
-	    for(int j=0; j<5; j++)
-	      for(int k=0; k<5; k++)
-		if(j==2 || k==2)
-		  for(int p=0; p<3; p++)
-		    input_image((int)descriptors[i].col+k-1, (int)descriptors[i].row+j-1, 0, p)=0;
-
-	  }
-
-	  input_image.get_normalize(0,255).save("sift.png");*/
+	for (int i=3;i<argc;i++){
+	  CImg<double> input_image2(argv[i]);
+	  // convert image to grayscale
+	  int count = match2Images(input_image,input_image2);
+	  struct matStr temp= {string(argv[i]),count};
+	  matC.push_back(temp);
+	}
+	sort(matC.begin(),matC.end(),sortImages);
+	vector<matStr>::iterator it;
+	cout<<matC.size()<<endl;
+	for(it = matC.begin();it != matC.end();it++){
+	  cout<<it->fileName<<endl;
+	}
+	cout<<endl;
 		
       }
     else if(part == "part2")
